135cf1e14c6405bc998390576cc478880d472fbc
Jiarr <jiarrdgr8@gmail.com>
2023-02-20 08:14:04 +0800

    match countdown done

--- src/Components/Right.js
+++ src/Components/Right.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import { useEffect, useState } from 'react';
 import axios from 'axios'
-// import { objectdata } from '../test.js'
-// import objectdata from '../test.txt'
+
 
 
 // const options = {
 //   method: 'GET',
-//   url: 'https://esportapi1.p.rapidapi.com/api/esport/matches/18/2/2023',
+//   url: 'https://esportapi1.p.rapidapi.com/api/esport/matches/20/2/2023',
 //   headers: {
 //     'X-RapidAPI-Key': '794bdf6fd2mshfdefe85d9c592e0p1293d9jsnf25ae765086e',
 //     'X-RapidAPI-Host': 'esportapi1.p.rapidapi.com'
@@ -18,18 +17,20 @@ import axios from 'axios'
 
 function Right() {
   const [dotaMatches, setDotaMatches] = useState([]);
+  const [currentTime, setCurrentTime] = useState(new Date());
+  let intervalId;
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.request('http://localhost:3000/file.txt');
-        // console.log(response.data.events);
-        const dotaMatches = response.data.events.filter((game) => {
+
+        const intervalId = setInterval(() => {
+          setCurrentTime(new Date())}, 5000)
+
+          const dotaMatches = response.data.events.filter((game) => {
           return game.tournament.category.flag === 'dota-2';
         });
-
-        // setDotaMatches(dotaMatches);
-        // console.log(dotaMatches);
 
         dotaMatches.sort((a, b) => {
           return a.startTimestamp - b.startTimestamp;
@@ -47,16 +48,23 @@ function Right() {
 
     fetchData();
 
-    return () => {
-      // cleanup logic goes here
-    };
+    return () => clearInterval(intervalId);
   }, []);
 
 
   return (
     <div>
       {dotaMatches.map((game) => (
-        <div key={game.customId}>{game.eventType}</div>
+        <div key={game.customId} className='flex justify-start items-center border border-gray-700 '>
+          <div className='flex max-w-xs items-center'>logo</div>
+          <div className='flex justify-center flex-grow flex flex-col '>
+            <div>{game.homeTeam.name} vs {game.awayTeam.name}</div>
+            <div>{game.tournament.uniqueTournament.name} {game.status.description}</div>
+          </div>
+          <div className='flex'>
+            {Math.floor((game.startTimestamp-(new Date().getTime()/1000))/3600) >= 1 && <p>{Math.floor((game.startTimestamp-(new Date().getTime()/1000))/3600)} H </p>} <p>{Math.round(((game.startTimestamp-(new Date().getTime()/1000))%3600)*(60/3600))} M</p>
+          </div>
+        </div>
       ))}
     </div>
   );
